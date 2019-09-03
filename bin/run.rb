af44a55e80e19c2cdf601b90d414997ae1f18c0c
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-03 17:57:06 +0100

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,23 @@
+require_relative '../config/environment'
+
 Hashie
 Sinatra
 Octokit
 Pry
 AwesomePrint
+
+
+
+# require 'mail'
+
+# gem 'nokogiri', :git => 'login@example.com:some-user-account/some-private-gem.git'
+
+# #Example gemfile:
+
+# source "https://rubygems.org"
+ 
+# gem "sinatra"
+ 
+# group :development do
+#   gem "pry"
+# end
